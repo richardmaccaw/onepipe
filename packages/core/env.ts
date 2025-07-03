42cd33f04ee3d0fdf35ea1a540469598130ad079
richardmaccaw <ricci6@gmail.com>
2025-07-03 09:53:00 +0200

    chore: remove oauth experiment

--- packages/core/env.ts
+++ packages/core/env.ts
@@ -4,13 +4,5 @@ export interface Env {
   TOKEN_CACHE: KVNamespace
   QUEUE: Queue<QueueMessage>
 
-  // Setup mode variables
-  SETUP_MODE?: string
-  SETUP_TOKEN?: string
-  
-  // OAuth configuration
-  GOOGLE_OAUTH_CLIENT_ID?: string
-  GOOGLE_OAUTH_CLIENT_SECRET?: string
-
   [key: string]: any
 }
